2a1684a3a712958b1ed109c21d31ecb6f1fdf701
Wen Niao Qu <wenniaoqu@wqmbair.mynetworksettings.com>
2025-02-07 18:51:05 -0500

    start project

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,3 +15,23 @@ export default {
   },
   plugins: [],
 };
+
+// tailwind.config.js
+const {heroui} = require("@heroui/react");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // ...
+    // make sure it's pointing to the ROOT node_module
+    "./src/app/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+};
