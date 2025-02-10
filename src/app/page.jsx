8bbe569854044e15dd2c9ffc0196570ae3da3e92
wennq <wqu1202@gmail.com>
2025-02-10 17:16:42 -0500

    add breed fetching and filtering ui

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,28 +1,102 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useRouter } from 'next/navigation';
+import { getBreeds } from "@/utils/api";
+import {Button, Form, Select, SelectItem, Input} from "@heroui/react";
 
 export default function Home() {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [dogBreeds, setDogBreeds] = useState();
+  const [selectedBreeds, setSelectedBreeds] = useState(new Set([]));
+  const [minAge, setMinAge] = useState();
+  const [maxAge, setMaxAge] = useState();
+  const [zipcode, setZipcode] = useState();
+  const [sortOrder, setSortOrder] = useState();
 
-  const [isAuth, setAuth] = useState(false);
-  const router = useRouter()
-  // useEffect(() => {
-  //   let data = sessionStorage.getItem("auth");
-  //   console.log(data);
-  //   const isAuthenticated = data ? true : false;
-  //   // If the user is authenticated, continue as normal
-  //   if(!isAuthenticated) {
-  //     router.push('/login');
-  //   }
-  //   else {
-  //     setAuth(true);
-  //   }
-  
-  // })
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setSubmitted(true);
+    setIsLoading(false);
+  }
+   useEffect(() => {
+    console.log('fetching breeds');
+    const fetchBreeds = async () => {
+       const data = await getBreeds();
+       setDogBreeds(data);
+    }
+    fetchBreeds();
+  }, []);
+
   return (
    <div>
-    <p>Home page</p>
-    <p>do not view without logging in</p>
+    <div className="hero">
+      <p>Start finding your match by filtering for a breed (or just browse all breeds!)</p>
+      <p>Saw a dog you like? Add them to your favorites</p>
+      <p>Once you are ready, click on Find my match and get matched.</p>
+    </div>
+    <div className="filter">
+    <Form className="w-full max-w-xs flex gap-4" onSubmit={handleSearch} validationBehavior="native">  
+      <Select
+        className="max-w-xs"
+        label="Filter by breed"
+        placeholder="All breeds"
+        selectionMode="multiple"
+        selectedKeys={selectedBreeds}
+        onSelectionChange={setSelectedBreeds}
+        >
+        {dogBreeds && dogBreeds.map((breed) => (
+          <SelectItem key={breed}>{breed}</SelectItem>
+        ))}
+      </Select>    
+      <Input
+          label="Minimum age"
+          placeholder="0"
+          type="number"
+          min="0"
+          value={minAge} onValueChange={setMinAge}
+        />
+         <Input
+          label="Maximum age"
+          placeholder="29"
+          type="number"
+          max="29"
+          value={maxAge} onValueChange={setMaxAge}
+
+        />
+        <Input
+          label="Filter by Zipcode"
+          placeholder="000000"
+          value={zipcode} onValueChange={setZipcode}
+        />
+        <Select className="max-w-xs" label="Sort breed by" placeholder="Ascending" defaultSelectedKeys={["ascending"]} onValueChange={setSortOrder}>
+            <SelectItem key="ascending">Ascending</SelectItem>
+            <SelectItem key="descending">Descending</SelectItem>
+        </Select>
+
+      <div className="flex gap-2">
+        <Button color="primary" type="submit" isLoading={isLoading ? true : false}>
+          Search
+        </Button>
+      </div>
+
+      {submitted && <>
+        <p className="text-small text-default-500">Selected: {Array.from(selectedBreeds).join(", ")}</p>
+        <p className="text-small text-default-500">Min age: {minAge}</p>
+        <p className="text-small text-default-500">Max age: {maxAge}</p>
+        <p className="text-small text-default-500">Zipcode: {zipcode}</p>
+      </> 
+}
+      {hasError && (
+        <div className="text-small text-default-500">
+          Oops something went wrong. Please try again later.
+        </div>
+      )}
+  </Form>  
+    </div>
    </div>
   );
 }
---
+++ src/utils/api.js
@@ -0,0 +1,18 @@
+const BASE_URL = "https://frontend-take-home-service.fetch.com";
+
+//get list of breeds
+export const getBreeds = async () => {
+    const response = await fetch(`${BASE_URL}/dogs/breeds`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+        })
+
+    if (response.ok) {
+        const dogBreeds = await response.json();
+        return dogBreeds;
+    } else {
+    // Handle errors
+    throw new Error(`Response status: ${response.status}`);
+    }
+}
